1c427818d7504a0efd5d75422961170f4036ae86
I-AM-EXCEPTIONAL <104685343+I-AM-EXCEPTIONAL@users.noreply.github.com>
2024-02-13 16:29:38 +0530

    Update settings.py

--- todoprj/settings.py
+++ todoprj/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    '**corsheaders.middleware.CorsMiddleware**',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
